2087468c89cf9d0e2d4e0646bdcecda5dc9cac69
Md.Asif <asibulislam062@gmail.com>
2025-08-30 14:28:47 +0600

    ph commit

---
+++ index.js
@@ -0,0 +1,324 @@
+document.getElementById('btn-1').addEventListener('click',function(e){
+e.preventDefault();
+const number=999;
+
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===999){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+const trans=[];
+
+
+setInnerText(total)
+  const data={
+    name:'last',
+    date:new Date().toLocaleDateString()
+  }  
+  
+trans.push(data)
+
+  console.log(trans)
+})
+
+document.getElementById('btn-2').addEventListener('click',function(e){
+e.preventDefault();
+const number=999;
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===999){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+    
+
+
+
+})
+
+document.getElementById('btn-3').addEventListener('click',function(e){
+e.preventDefault();
+const number=999;
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===999){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+    
+
+
+
+})
+
+document.getElementById('btn-4').addEventListener('click',function(e){
+e.preventDefault();
+const number=1994-999999;
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===1994-999999){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+    
+
+
+
+})
+
+document.getElementById('btn-5').addEventListener('click',function(e){
+e.preventDefault();
+const number=109;
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===109){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+    
+
+
+
+})
+document.getElementById('btn-6').addEventListener('click',function(e){
+e.preventDefault();
+const number=106;
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===106){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+    
+
+
+
+})
+
+document.getElementById('btn-7').addEventListener('click',function(e){
+e.preventDefault();
+const number=1994-999999;
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===1994-999999){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+})
+document.getElementById('btn-8').addEventListener('click',function(e){
+e.preventDefault();
+const number=1994-999999;
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===1994-999999){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+    
+
+
+
+})
+document.getElementById('btn-9').addEventListener('click',function(e){
+e.preventDefault();
+const number=1994-999999;
+const coin=document.getElementById('coin').innerText;
+const coinValue=parseInt(coin);
+const total=coin-20;
+document.getElementById('coin').innerText=total
+if(number===1994-999999){
+    
+// console.log(total);
+    alert('your 20 coins cut');
+    
+
+}
+
+
+if(total<20){
+    alert('no call place');
+}
+
+    
+
+
+
+})
+
+
+
+
+
+
+
+document.getElementById("number-1").addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById("num1").innerText);
+if(num===999){
+    alert('National Emergency 999')
+}
+})
+
+document.getElementById('number-2').addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById('num2').innerText);
+if(num===999){
+    alert('Police 999');
+}
+})
+
+document.getElementById('number-3').addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById('num3').innerText);
+if(num===999){
+    alert('Fire Service 999');
+}
+})
+
+document.getElementById('number-4').addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById('num4').innerText);
+if(num===1994-999999){
+    alert('Ambulance 1994-999999');
+}
+})
+
+document.getElementById('number-5').addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById('num5').innerText);
+if(num===109){
+    alert('Women & Child Helpline 109');
+}
+})
+
+document.getElementById('number-6').addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById('num6').innerText);
+if(num===109){
+    alert('Anti-Corruption 106');
+}
+})
+
+document.getElementById('number-7').addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById('num7').innerText);
+if(num===106){
+    alert('Electricity Outage 16216');
+}
+})
+
+document.getElementById('number-8').addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById('num8').innerText);
+if(num===16216){
+    alert('Brac 16445');
+}
+})
+
+document.getElementById('number-9').addEventListener('click',function(e){
+e.preventDefault();
+// const num=999;
+const num=parseInt(document.getElementById('num9').innerText);
+if(num===16216){
+    alert('Bangladesh Railway 163');
+}
+})
+
+
